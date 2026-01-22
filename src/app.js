2cdc4dce5c4390168ee2d28173c5a63306191218
Marti-Ferrer <ferrermarti9@gmail.com>
2026-01-22 20:45:19 +0000

    First fommit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,55 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+
+
+
+
+
+function generarExcusa() {
+  const sujetos = [
+    "Mi perro",
+    "El vecino",
+    "Mi computadora",
+    "El internet",
+    "El despertador",
+    "Mi jefe"
+  ];
+
+  const acciones = [
+    "se comió",
+    "rompió",
+    "borró",
+    "perdió",
+    "apagó",
+    "mojó"
+  ];
+
+  const objetos = [
+    "mi tarea",
+    "el archivo",
+    "el proyecto",
+    "el correo",
+    "mi teléfono",
+    "las llaves"
+  ];
+
+  const cuando = [
+    "mientras dormia", 
+    "cuando estaba en clase", 
+    "cuando hacia ejercicio", 
+    "jugando", 
+    "mientras me duchaba"
+  ];
+
+  const sujeto = sujetos[Math.floor(Math.random() * sujetos.length)];
+  const accion = acciones[Math.floor(Math.random() * acciones.length)];
+  const objeto = objetos[Math.floor(Math.random() * objetos.length)];
+  const cuando = cuando[Math.floor(Math.random() * cuando.length)];
+
+  return `${sujeto} ${accion} ${objeto} ${cuando}.`;
+}
+
+// Ejemplo de uso
+console.log(generarExcusa());
