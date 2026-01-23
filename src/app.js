7a63e86efb9aee1a48c39a4c7efe1064112cea22
Marti-Ferrer <ferrermarti9@gmail.com>
2026-01-23 19:59:24 +0000

    Final round

--- src/app.js
+++ src/app.js
@@ -6,8 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+
+const randomElement = (Array)=>{
+  const randomPosition = Math.trunc(Math.random() * Array.length)
+  return Array[randomPosition]
+} 
+const excuse = randomElement(who) + " " + randomElement(action) + " " + randomElement(what) + " " + randomElement(when) + " " + "."
+
+const excuseHTMLElement = document.getElementById("excuse")
+
+excuseHTMLElement.innerHTML = excuse
+
 };
 
 
@@ -15,49 +29,30 @@ window.onload = function() {
 
 
 
-function generarExcusa() {
-  const sujetos = [
-    "Mi perro",
-    "El vecino",
-    "Mi computadora",
-    "El internet",
-    "El despertador",
-    "Mi jefe"
-  ];
 
-  const acciones = [
-    "se comió",
-    "rompió",
-    "borró",
-    "perdió",
-    "apagó",
-    "mojó"
-  ];
 
-  const objetos = [
-    "mi tarea",
-    "el archivo",
-    "el proyecto",
-    "el correo",
-    "mi teléfono",
-    "las llaves"
-  ];
 
-  const cuando = [
-    "mientras dormia", 
-    "cuando estaba en clase", 
-    "cuando hacia ejercicio", 
-    "jugando", 
-    "mientras me duchaba"
-  ];
 
-  const sujeto = sujetos[Math.floor(Math.random() * sujetos.length)];
-  const accion = acciones[Math.floor(Math.random() * acciones.length)];
-  const objeto = objetos[Math.floor(Math.random() * objetos.length)];
-  const cuando = cuando[Math.floor(Math.random() * cuando.length)];
 
-  return `${sujeto} ${accion} ${objeto} ${cuando}.`;
-}
 
-// Ejemplo de uso
-console.log(generarExcusa());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
